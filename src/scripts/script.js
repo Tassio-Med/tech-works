fd3e3373b85f051308e7b0a6c86afb9209e817ac
Tassio-Med <tassiohfmed@gmail.como>
2023-04-09 18:52:45 -0300

    :sparkles: feat: requisição à api de blogs feita

---
+++ src/scripts/script.js
@@ -0,0 +1,37 @@
+const url = "https://jsonplaceholder.typicode.com/posts?_limit=5";
+
+const loadingElement = document.querySelector("#loading");
+const postsContainer = document.querySelector("#posts-container");
+
+async function  getAllPosts() {
+  const response = await fetch(url);
+
+  console.log(response);
+
+  const data = await response.json();
+
+  console.log(data);
+
+  loadingElement.classList.add("hide");
+
+  data.map((post) => {
+    
+    const div = document.createElement("div")
+    const title = document.createElement("h2")
+    const body = document.createElement("p")
+    const link = document.createElement("a")
+
+    title.innerText = post.title;
+    body.innerText = post.body;
+    link.innerText = "Ler";
+    link.setAttribute("href", `post.html?id=${post.id}`);
+
+    div.appendChild(title);
+    div.appendChild(body);
+    div.appendChild(link);
+
+    postsContainer.appendChild(div);
+  })
+}
+
+getAllPosts();
